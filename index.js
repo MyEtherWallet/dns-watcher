52ffd978ff9f5ea4c09780481428ff44a3c5d960
Gage Siuniak <gsiuniak@gmail.com>
2018-11-15 15:21:56 -0800

    going to start proxy

--- index.js
+++ index.js
@@ -3,5 +3,7 @@
 // See: https://www.npmjs.com/package/module-alias //
 require('module-alias/register')
 
+// https://stackoverflow.com/questions/10777657/node-js-dns-lookup-how-to-set-timeout //
+
 const hc = require('@lib/health-check')
 hc.init()
