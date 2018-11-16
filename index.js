ef5b2b2e1541eb0e71958bc4cfcf6e4e9ced2f11
Gage Siuniak <gsiuniak@gmail.com>
2018-11-16 12:06:49 -0800

    Added json generator

--- index.js
+++ index.js
@@ -4,14 +4,22 @@
 require('module-alias/register')
 
 // https://stackoverflow.com/questions/10777657/node-js-dns-lookup-how-to-set-timeout //
-//const hc = require('@lib/health-check')
-//hc.init()
-//
-const rs = require('@lib/redis-store')
+const hc = require('@lib/health-check')
+// hc.init()
+
+// const rs = require('@lib/redis-store')
+
+// async function b() {
+//   let a = await rs.getAllNameServerStatus()
+//   console.log(a)
+// }
+
+// b()
+// 
+const jg = require('@lib/json-generator')
 
 async function b() {
-  let a = await rs.getAllNameServerStatus()
-  console.log(a)
+  await jg.init()
 }
 
 b()
