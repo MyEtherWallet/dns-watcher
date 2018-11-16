a37a2c9cfead0eef0000033f4650d80e9c363ebc
Gage Siuniak <gsiuniak@gmail.com>
2018-11-16 13:14:02 -0800

    emitter works

--- index.js
+++ index.js
@@ -5,21 +5,18 @@ require('module-alias/register')
 
 // https://stackoverflow.com/questions/10777657/node-js-dns-lookup-how-to-set-timeout //
 const hc = require('@lib/health-check')
-// hc.init()
-
-// const rs = require('@lib/redis-store')
-
-// async function b() {
-//   let a = await rs.getAllNameServerStatus()
-//   console.log(a)
-// }
-
-// b()
-// 
 const jg = require('@lib/json-generator')
 
-async function b() {
-  await jg.init()
+function init() {
+  hc.init()
 }
 
-b()
+hc.emitter.on('end', async () => {
+  console.log('\n Updating JSON list...')
+  await jg.init()
+  init()
+})
+
+init()
+
+
