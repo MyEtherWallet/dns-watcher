70d0630d347d46c5ab9745bc70dc2bfa24c0cabf
Gage Siuniak <gsiuniak@gmail.com>
2018-11-16 11:30:20 -0800

    changed format of redis store, added getAll function. starting json-generrator

--- index.js
+++ index.js
@@ -4,6 +4,14 @@
 require('module-alias/register')
 
 // https://stackoverflow.com/questions/10777657/node-js-dns-lookup-how-to-set-timeout //
-const hc = require('@lib/health-check')
-hc.init()
+//const hc = require('@lib/health-check')
+//hc.init()
+//
+const rs = require('@lib/redis-store')
 
+async function b() {
+  let a = await rs.getAllNameServerStatus()
+  console.log(a)
+}
+
+b()
