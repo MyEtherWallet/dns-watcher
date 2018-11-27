b77ed3d6c0fbf0fdcf2a12687635fb7b5f5549fc
Gage Siuniak <gsiuniak@gmail.com>
2018-11-27 12:23:01 -0800

    more style changes

--- frontend/src/components/FrontEnd.vue
+++ frontend/src/components/FrontEnd.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="frontend">
 
-    <header class="header">
-      <div class="page-container header-elements-container">
-        <div class="logo-image">
-          <img src="https://www.myetherwallet.com/images/logo-myetherwallet.svg" alt="">
+    <div class="top">
+      <header class="header">
+        <div class="page-container header-elements-container">
+          <div class="logo-image">
+            <img src="https://www.myetherwallet.com/images/logo-myetherwallet.svg" alt="">
+          </div>
         </div>
-      </div>
-    </header>
-    <file-check/>
+      </header>
+      <file-check/>
 
-    <div class="buttons-container">
-      <div class="page-container">
-        <div class="buttons flex-box-item-center">
-          <h3 v-bind:class="[buttonClass, (currentFilter === 2) ? selectedClass : '']"
-              v-on:click="filterEntries(2)">All</h3>
-          <h3 v-bind:class="[buttonClass, (currentFilter === 3) ? selectedClass : '']"
-              v-on:click="filterEntries(3)">Errors</h3>
-          <h3 v-bind:class="[buttonClass, (currentFilter === 4) ? selectedClass : '']"
-              v-on:click="filterEntries(4)">Valid</h3>
+      <div class="buttons-container">
+        <div class="page-container">
+          <div class="buttons flex-box-item-center">
+            <h3 v-bind:class="[buttonClass, (currentFilter === 2) ? selectedClass : '']"
+                v-on:click="filterEntries(2)">All</h3>
+            <h3 v-bind:class="[buttonClass, (currentFilter === 3) ? selectedClass : '']"
+                v-on:click="filterEntries(3)">Errors</h3>
+            <h3 v-bind:class="[buttonClass, (currentFilter === 4) ? selectedClass : '']"
+                v-on:click="filterEntries(4)">Valid</h3>
+          </div>
         </div>
-      </div>
+      </div>      
     </div>
 
     <div class="dns-table">
