8a3f94ccc0bbd24b38884e6d4efdca0d12af7016
Gage Siuniak <gsiuniak@gmail.com>
2018-11-30 12:16:16 -0800

    Production server, etc, all done

--- README.md
+++ README.md
@@ -17,6 +17,9 @@ sudo apt install redis-server
 Edit the .env file located in the root of the project
 
 ```
+# Port to run on
+PORT=8080
+
 # Domain to check
 DOMAIN=myetherwallet.com
 
@@ -31,23 +34,37 @@ GITHUB_SITE=https://raw.githubusercontent.com/kvhnuke/etherwallet/gh-pages
 
 # Telegram Info
 TELEGRAM_KEY=key_here
-TELEGRAM_CHAT_ID=chat_id_here
+TELEGRAM_CHAT_ID=-290489995
 ```
 
 ## Usage
 
-### DNS Check
+### Start DNS Check
 
 ```bash
 npm run start:watcher
 ```
 
-### Development Frontend Server
+### Start Development Frontend Server
 
 ```bash
 npm run start:dev
 ```
 
+### Build Production Frontend
+
+```bash
+npm run build:prod
+```
+
+### Start Production Frontend
+
+```bash
+npm run start:prod
+```
+
 ## References
 
+The `package.json` file has some options: RES_OPTIONS='retrans:2500 retry:2'
+This is to override some default options when looking up DNS records. See:
 https://stackoverflow.com/questions/10777657/node-js-dns-lookup-how-to-set-timeout
--- frontend/web-server.js
+++ frontend/web-server.js
@@ -1,19 +1,29 @@
 'use strict'
 
-const path = require('path')
-// const ENV_PATH = path.join(__dirname, '../.env')
-// require('dotenv').config({ path: ENV_PATH })
-var superstatic = require('superstatic').server;
+// See: https://www.npmjs.com/package/module-alias //
+require('module-alias/register')
 
+// Imports //
+const path = require('path')
+const superstatic = require('superstatic')
+const connect = require('connect')
+const _ = require('underscore')
+const ENV_PATH = path.join(__dirname, '../.env')
+require('dotenv').config({ path: ENV_PATH })
+
+// Lib //
+const redisStore = require('@lib/redis-store')
+ 
+// Superstatic Options //
 var spec = {
-  port: 8080,
+  port: process.env.PORT,
   'trailingSlash': false,
   compression: true,
   cwd: __dirname + '/dist',
   errorPage: __dirname + '/error.html',
   config: {
     'rewrites': [
-      {'source': '/dns-report', 'destination': './status-list.json'}
+      // {'source': '/dns-report', 'destination': '/dns-report'}
     ],
     "headers": [
       {
@@ -29,8 +39,23 @@ var spec = {
   }
 };
 
-var app = superstatic(spec);
+// Create Connect App //
+var app = connect()
 
-app.listen(function() {
-  console.log('Website running on port 8080.  http://localhost:8080');
-});
+// Override /dns-report route //
+app.use('/dns-report', async (req, res, next) => {
+  let entries = await redisStore.default.getAllNameServerStatus()
+  let sorted_by_date = _.sortBy(entries, function(o) { return - (new Date(o.timestamp) )})
+  let sorted_by_status = sorted_by_date.sort((a, b) => a.status - b.status)
+  let json_string = JSON.stringify(sorted_by_status)
+  return res.end(json_string)
+})
+
+// Use superstatic to handle other routes //
+app.use(superstatic(spec))
+
+// Start Server //
+app.listen(process.env.PORT, function() {
+  console.log('Website running on port 8080.  http://localhost:8080')
+})
+
