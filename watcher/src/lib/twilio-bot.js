f3180ae547aa4106eb281bc97b1ca23a7260ac20
Gage <gsiuniak@gmail.com>
2019-07-08 14:33:32 -0700

    added ability to have multiple numbers for phone alert

--- watcher/src/lib/twilio-bot.js
+++ watcher/src/lib/twilio-bot.js
@@ -1,6 +1,7 @@
 'use strict'
 
 // Imports //
+import async from 'async'
 import request from 'request-promise-native'
 import redisStore from '@lib/redis-store'
 import twilio from 'twilio'
@@ -10,18 +11,37 @@ const client = twilio(process.env.TWILIO_ID, process.env.TWILIO_TOKEN)
 export default (() => {
   
   /**
-   *  Use Twilio to call and notify a number (defined in .env)
+   *  Use Twilio to call and notify command-separated number lists (defined in .env)
    */
   const phoneAlert = async () => {
     return new Promise(async (resolve, reject) => {
       // If message was sent within the past 30minutes, do not send //
-      // let canCall = await checkCallStatus('twilio-call-alert')
-      // if (!canCall) return resolve()
+      let canCall = await checkCallStatus('twilio-call-alert')
+      if (!canCall) return resolve()
+
+      const numbers = process.env.TWILIO_TO_NUMBER.split(',')
+      async.each(numbers, async (number, done) => {
+        await makeCall(number)
+        done()
+      }, () => {
+        resolve()
+      })
+      
+    })
+  }
+
+  /**
+   * Make call to particular number, iterated from phoneAlert()
+   *
+   * @param  {String} number - Number to call
+   */
+  const makeCall = async (number) => {
+    return new Promise(async (resolve, reject) => {
 
       client.calls
         .create({
           url: `https://status.myetherwallet.com/voice-alert`,
-          to: process.env.TWILIO_TO_NUMBER,
+          to: number,
           from: process.env.TWILIO_FROM_NUMBER,
           method: 'GET'
         })
